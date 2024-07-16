4a6fda9b34196c945b1ebb9ad57a31158244f425
kwang <kwang@rollong.com>
2024-07-16 22:32:22 +0800

    build: add doc for mvn install

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # a simple quiz code
 
+## environments
+
 build tools: Apache Maven 3.9.6
 
 jdk: semeru-openj9 11.0.19+7
+
+## test me
+
+using JUnit test case or
+
+```shell
+mvn install
+java -jar ./target/quiz-1.0-SNAPSHOT.jar abc123.456 cc123 2c.-3.1 3.21cc
+
+outpus:
+123.45
+123.00
+-3.10
+failed to extract decimal from:3.21cc
+```
+
--- src/main/java/com/example/quiz/Main.java
+++ src/main/java/com/example/quiz/Main.java
@@ -2,9 +2,13 @@ package com.example.quiz;
 
 public class Main {
 
-    public static void main(String... args) throws StripUtils.ExtractionFailedException {
-        for (final String s : args) {
-            System.out.printf(StripUtils.getRSBDecimal(s));
+    public static void main(String... args) {
+        try {
+            for (final String s : args) {
+                System.out.println(StripUtils.getRSBDecimal(s));
+            }
+        } catch (final StripUtils.ExtractionFailedException ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
